7eb5c2fad52a73f8f2d9b6e6116da270399bcd89
Aerafal <jimmy.mll@hotmail.com>
2023-11-01 22:21:39 +0100

    feat(core): add network transports

---
+++ libs/Synthesis.Core/Network/Transport/DofusServer.cs
@@ -0,0 +1,79 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Microsoft.Extensions.Logging;
+using Synthesis.Core.Network.Dispatcher;
+using Synthesis.Core.Network.Framing;
+
+namespace Synthesis.Core.Network.Transport;
+
+/// <summary>
+/// Represents a Dofus server capable of handling multiple sessions over TCP connections.
+/// </summary>
+/// <typeparam name="TSession">The type of Dofus session associated with the server.</typeparam>
+public abstract class DofusServer<TSession>(ILoggerFactory loggerFactory, IMessageDecoder decoder, IMessageEncoder encoder, IMessageDispatcher dispatcher)
+    where TSession : DofusSession
+{
+    private readonly CancellationTokenSource _cts = new();
+    private readonly ILogger<DofusServer<TSession>> _logger = loggerFactory.CreateLogger<DofusServer<TSession>>();
+    private readonly Socket _socket = new(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+    /// <summary>
+    /// Starts the Dofus server and listens on the specified port(s) for incoming connections.
+    /// </summary>
+    /// <param name="ports">The port(s) to bind and listen for incoming connections.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public async Task StartAsync(params int[] ports)
+    {
+        try
+        {
+            foreach (var port in ports)
+                _socket.Bind(new IPEndPoint(IPAddress.Any, port));
+        }
+        catch (SocketException e) when (e.SocketErrorCode is SocketError.AddressAlreadyInUse)
+        {
+            _logger.LogError(e, "Failed to bind to port: {Message}", e.Message);
+            return;
+        }
+
+        _socket.Listen();
+
+        while (!_cts.IsCancellationRequested)
+        {
+            var socket = await _socket.AcceptAsync().ConfigureAwait(false);
+
+            var session = CreateSession(socket, decoder, encoder, dispatcher);
+
+            _ = OnSessionConnectedAsync(session)
+                .ContinueWith(_ => session.StartAsync())
+                .Unwrap()
+                .ContinueWith(_ => session.Dispose())
+                .ContinueWith(_ => OnSessionDisconnectedAsync(session))
+                .Unwrap()
+                .ConfigureAwait(false);
+        }
+    }
+
+    /// <summary>
+    /// Creates a new Dofus session based on the provided socket, decoder, encoder, and dispatcher.
+    /// </summary>
+    /// <param name="socket">The <see cref="Socket"/> associated with the session.</param>
+    /// <param name="decoder">The message decoder for the session.</param>
+    /// <param name="encoder">The message encoder for the session.</param>
+    /// <param name="dispatcher">The message dispatcher for the session.</param>
+    /// <returns>An instance of the Dofus session.</returns>
+    protected abstract TSession CreateSession(Socket socket, IMessageDecoder decoder, IMessageEncoder encoder, IMessageDispatcher dispatcher);
+
+    /// <summary>
+    /// Invoked when a new Dofus session is successfully connected.
+    /// </summary>
+    /// <param name="session">The connected Dofus session.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    protected abstract Task OnSessionConnectedAsync(TSession session);
+
+    /// <summary>
+    /// Invoked when a Dofus session is disconnected.
+    /// </summary>
+    /// <param name="session">The disconnected Dofus session.</param>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    protected abstract Task OnSessionDisconnectedAsync(TSession session);
+}
---
+++ libs/Synthesis.Core/Network/Transport/DofusSession.cs
@@ -0,0 +1,120 @@
+﻿using System.Buffers;
+using System.Net.Sockets;
+using Synthesis.Core.Network.Dispatcher;
+using Synthesis.Core.Network.Framing;
+using Synthesis.Core.Network.Metadata;
+
+namespace Synthesis.Core.Network.Transport;
+
+/// <summary>
+/// Represents a session for handling Dofus protocol messages over a socket connection.
+/// </summary>
+public abstract class DofusSession(Socket socket, IMessageDecoder decoder, IMessageEncoder encoder, IMessageDispatcher dispatcher) : IDisposable
+{
+    private readonly CancellationTokenSource _cts = new();
+    private readonly MemoryPool<byte> _memoryPool = MemoryPool<byte>.Shared;
+    private bool _disposed;
+
+    private string? _sessionId;
+
+    /// <summary>
+    /// Gets the unique identifier for the session.
+    /// </summary>
+    public string SessionId =>
+        _sessionId ??= Guid.NewGuid().ToString("N");
+
+    /// <summary>
+    /// Gets a cancellation token associated with the session.
+    /// </summary>
+    public CancellationToken CancellationToken =>
+        _cts.Token;
+
+    /// <summary>
+    /// Starts the Dofus session to receive and process incoming messages.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    internal async Task StartAsync()
+    {
+        while (!_cts.IsCancellationRequested)
+        {
+            using var owner = _memoryPool.Rent();
+
+            var bytesRead = await socket.ReceiveAsync(owner.Memory, SocketFlags.None, CancellationToken).ConfigureAwait(false);
+
+            if (bytesRead is 0)
+                break;
+
+            var buffer = owner.Memory[..bytesRead];
+
+            while (decoder.TryDecode(ref buffer, out var message))
+                await dispatcher.DispatchAsync(this, message).ConfigureAwait(false);
+        }
+    }
+
+    /// <summary>
+    /// Sends a specific type of Dofus message asynchronously.
+    /// </summary>
+    /// <typeparam name="TMessage">The type of Dofus message to send.</typeparam>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
+    public ValueTask SendAsync<TMessage>() where TMessage : DofusMessage, new()
+    {
+        return SendAsync(new TMessage());
+    }
+
+    /// <summary>
+    /// Sends a Dofus message asynchronously.
+    /// </summary>
+    /// <param name="message">The Dofus message to send.</param>
+    /// <returns>A <see cref="ValueTask"/> representing the asynchronous operation.</returns>
+    public ValueTask SendAsync(DofusMessage message)
+    {
+        var buffer = encoder.Encode(message);
+
+        var task = socket.SendAsync(buffer, SocketFlags.None, CancellationToken);
+
+        return task.IsCompletedSuccessfully
+            ? ValueTask.CompletedTask
+            : FlushAsync(task);
+
+        static async ValueTask FlushAsync(ValueTask<int> flushTask)
+        {
+            await flushTask.ConfigureAwait(false);
+        }
+    }
+
+    /// <summary>
+    /// Disconnects the Dofus session asynchronously.
+    /// </summary>
+    /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+    public Task DisconnectAsync()
+    {
+        return _cts.CancelAsync();
+    }
+    
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+
+        _disposed = true;
+
+        if (!_cts.IsCancellationRequested)
+            _cts.Cancel();
+
+        try
+        {
+            socket.Shutdown(SocketShutdown.Both);
+        }
+        catch (SocketException)
+        {
+            // ignored
+        }
+
+        socket.Close();
+        socket.Dispose();
+        _cts.Dispose();
+
+        GC.SuppressFinalize(this);
+    }
+}
