07c14155033762a30676c581c80cb33009ef4b8b
Aerafal <jimmy.mll@hotmail.com>
2023-11-01 21:01:20 +0100

    feat(core): add singleton for single instances

---
+++ libs/Synthesis.Core/Services/Singleton.cs
@@ -0,0 +1,17 @@
+﻿namespace Synthesis.Core.Services;
+
+/// <summary>
+/// Represents a generic singleton pattern implementation.
+/// </summary>
+/// <typeparam name="T">The type of the singleton instance.</typeparam>
+public abstract class Singleton<T>
+    where T : class, new()
+{
+    private static readonly Lazy<T> LazyInstance = new(() => new T(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+    /// <summary>
+    /// Gets the single instance of the <typeparamref name="T"/> type.
+    /// </summary>
+    public static T Instance =>
+        LazyInstance.Value;
+}
